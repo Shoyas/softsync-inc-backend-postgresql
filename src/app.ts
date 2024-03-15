6309f54009688628fa32419c6c7d5c46e9750db0
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2024-03-15 23:09:30 +0600

    [VERCEL] vercel deploy try

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000' ||
-        'https://softsyncinc.com/' ||
+        'https://softsyncinc.com' ||
         'https://www.softsyncinc.com' ||
         'http://softsyncinc.com',
     ],
