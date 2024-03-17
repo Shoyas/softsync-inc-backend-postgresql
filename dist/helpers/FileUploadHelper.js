74496213700807ea93decb5954f1588d3c8a2707
Md. Nasir Uddin <67519258+Shoyas@users.noreply.github.com>
2024-03-17 12:37:12 +0600

    Update FileUploadHelper.js
    For trying fix file upload directory.

--- dist/helpers/FileUploadHelper.js
+++ dist/helpers/FileUploadHelper.js
@@ -51,7 +51,7 @@ cloudinary_1.v2.config({
 // });
 const storage = multer_1.default.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+        cb(null, './uploads/');
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
